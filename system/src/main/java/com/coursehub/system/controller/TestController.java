444eae938e1424e681d3625cfc344d1ba9d01ff2
Minix93 <mxjason93@gmail.com>
2022-09-13 10:17:37 -0700

    1. add Route Forwarding configuration to gateway module, and tested without bugs

--- system/src/main/java/com/coursehub/system/controller/TestController.java
+++ system/src/main/java/com/coursehub/system/controller/TestController.java
@@ -3,6 +3,7 @@ package com.coursehub.system.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class TestController {
 
