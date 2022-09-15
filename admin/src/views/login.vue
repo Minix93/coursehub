ee28fbb9dc97718afa137973dbbccd525f1b51a3
Minix93 <mxjason93@gmail.com>
2022-09-15 14:08:33 -0700

    1. add login button event to redirect to admin page

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -50,7 +50,9 @@
                             <span class="lbl"> Remember Me</span>
                           </label>
 
-                          <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
+                          <button type="button"
+                                  class="width-35 pull-right btn btn-sm btn-primary"
+                                  v-on:click="login()">
                             <i class="ace-icon fa fa-key"></i>
                             <span class="bigger-110">Login</span>
                           </button>
@@ -208,6 +210,11 @@
 <script>
 $('body').attr('class', 'login-layout light-login');
 export default {
-  name: 'login'
+  name: 'login',
+  methods:{
+    login(){
+      this.$router.push("/admin")
+    }
+  }
 }
 </script>
