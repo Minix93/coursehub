26efca822f2b015433ebdb7a27c408027a97b0da
Minix93 <mxjason93@gmail.com>
2022-09-15 17:07:31 -0700

    1. add business module, add initial code

---
+++ business/src/main/java/com/coursehub/business/config/BusinessApplication.java
@@ -0,0 +1,31 @@
+package com.coursehub.business.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+@EnableEurekaClient
+@ComponentScan("com.coursehub")
+@MapperScan("com.coursehub.server.mapper")
+public class BusinessApplication {
+
+	private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
+
+	public static void main(String[] args) {
+
+		//SpringApplication.run(EurekaApplication.class, args);
+
+		SpringApplication app = new SpringApplication(BusinessApplication.class);
+		ConfigurableEnvironment env = app.run(args).getEnvironment();
+		LOG.info("Successfully started!!");
+		LOG.info("business address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+
+	}
+
+}
