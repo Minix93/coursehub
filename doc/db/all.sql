c2b36bb0e28d75cb4ad0dca9534e099c6b285d63
Minix93 <mxjason93@gmail.com>
2022-09-20 17:20:28 -0700

    1. add chapter table, generate persistant layer code

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -8,3 +8,13 @@ create table `test` (
 insert into `test` (id, name) VALUES (1, 'test');
 
 insert into `test` (id, name) VALUES (2, 'test2');
+
+
+
+drop table if exists `chapter`;
+create table `chapter` (
+    `id` char(8) not null comment 'ID',
+    `course_id` char(8) comment 'CourseID',
+    `name` varchar(50) comment 'name',
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment ='Outline';
