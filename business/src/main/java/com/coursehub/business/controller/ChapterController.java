8ab6170ee0d0e1701bec4f54887e8b3e5fb77f34
Minix93 <mxjason93@gmail.com>
2022-09-20 17:31:28 -0700

    1. add the query interface of chapter table

---
+++ business/src/main/java/com/coursehub/business/controller/ChapterController.java
@@ -0,0 +1,23 @@
+package com.coursehub.business.controller;
+
+import com.coursehub.server.domain.Chapter;
+import com.coursehub.server.service.ChapterService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+
+@RestController
+public class ChapterController {
+
+    @Resource
+    private ChapterService chapterService;
+
+    @RequestMapping("/chapter")
+    public List<Chapter> chapter(){
+
+        return chapterService.list();
+    }
+}
